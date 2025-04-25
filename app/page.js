22805384424a3ac7840967fb9116e86e9fc128b3
HarshaFdo <fernando.harsha2016@gmail.com>
2025-04-25 08:48:10 +0530

    login page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Hi</h2>
+      <Button variant="destructive">Go Back</Button>
+      <UserButton/>
     </div>
   );
 }
